7dfec66ae2221dbccbc682ca28480ede73be8aff
problem2.cpp <m.musashayxov@newuu.uz>
2024-09-16 19:45:17 +0500

    init

---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+
+// //PROBLEM 1
+// int main() {
+//     int a;cout<<"a:"<<endl;cin>>a;
+//     int b;cout<<"b:"<<endl;cin>>b;
+//     int c;cout<<"c:"<<endl;cin>>c;
+//     cout<<a-b-c<<endl;
+//     cout<<b*c-a<<endl;
+//     cout<<2*b-c+5*a<<endl;
+//     return 0;}
+
+
+// //PROBLEM 2
+// int main() {
+//     int d1;
+//     cout<<"First diagonal:"<<endl;
+//     cin>>d1;
+//     int d2;
+//     cout<<"Second diagonal:"<<endl;
+//     cin>>d2;
+//     cout<<"Area of rhombus:"<<(float)d1*d2/2<<endl;
+//     return 0;}
+
+
+
+//PROBLEM 3
+// int main() {
+//     float celsius;
+//     cout << "The temperature in Celsius:" << endl;
+//     cin>>celsius;
+//     float farenheit=celsius*9/5+32;
+//     cout <<"The temperature in Farenheit is equal to "<<farenheit<<endl;
+//     return 0;}
+
+
+
+//PROBLEM 4
+// int main() {
+//     int students;
+//     cout << "All number of students: "<<endl;
+//     cin >> students;
+//     int remain=students%50;
+//     if (remain==0) {
+//         cout<<0<<endl;}
+//     else
+//     cout <<50-remain<<endl;
+//     return 0;}
+
+
+//PROBLEM 5
+// int main() {
+//     float cm;
+//     cout<<"Sardor's height in centimeters:"<<endl;
+//     cin>>cm;
+//     float ft;
+//     cout<<"Mike's height in feet:"<<endl;
+//     cin>>ft;
+//     if (cm>ft*30.48)
+//         cout<<1<<endl;
+//     else
+//         cout<<0<<endl;
+//     return 0;}
